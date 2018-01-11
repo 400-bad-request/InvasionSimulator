39805b478e61eaca85fc42d1b83a3c0e0f25c300
SkalskiP <pskalski@student.agh.edu.pl>
2018-01-11 21:35:25 +0100

    Details view done.

--- src/sample/viewcontroller/CreditsController.java
+++ src/sample/viewcontroller/CreditsController.java
@@ -1,17 +1,34 @@
 package sample.viewcontroller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class CreditsController {
+
+    @FXML
+    private VBox leftBox;
+    @FXML
+    private VBox rightBox;
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    void initialize() {
+//        Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
+//        leftBox.setPrefWidth((screenSize.getWidth() - 70)/2);
+//        rightBox.setPrefWidth((screenSize.getWidth() - 70)/2);
+    }
 
     public void backToIntro(ActionEvent actionEvent) {
         try {
